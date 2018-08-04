d6f634724ba3b283e05c3750ee5defdbb90e2efe
Fangyan SUN <zhzxsunfangyan@yeah.net>
2018-08-04 17:17:18 +0200

    validat the form input to avoid conscious or inconscious cyber attack
    Signed-off-by: Fangyan SUN <zhzxsunfangyan@yeah.net>

--- crimemap.py
+++ crimemap.py
@@ -1,3 +1,5 @@
+import datetime
+import dateparser
 import dbconfig
 if dbconfig.test:
 	from mockdbhelper import MockDBHelper as DBHelper
@@ -9,19 +11,18 @@ from flask import Flask
 import json
 from flask import render_template
 from flask import request
+import string
+
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['mugging', 'break-in']
 
 @app.route("/")
-def home():
-	try:
-		crimes = DB.get_all_crimes()
-		crimes = json.dumps(crimes)
-	except Exception as e:
-		print e
-		data = None
-	return render_template("home.html", crimes = crimes, route_prefix = route_prefix)
+def home(error_message=None):
+	crimes = DB.get_all_crimes()
+	crimes = json.dumps(crimes)
+	return render_template("home.html", crimes = crimes, categories = categories, error_message = error_message, route_prefix = route_prefix)
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -43,12 +44,30 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
 	category = request.form.get("category")
-	date = request.form.get("date")
-	latitude = float(request.form.get("latitude"))
-	longitude = float(request.form.get("longitude"))
-	description = request.form.get("description")
+	if category not in categories:
+		return home()
+	date = format_date(request.form.get("date"))
+	if not date:
+		return home("Invalid date. Please use yyyy-mm-dd format")
+	try:
+		latitude = float(request.form.get("latitude"))
+		longitude = float(request.form.get("longitude"))
+	except ValueError:
+		return home()
+	description = sanitize_string(request.form.get("description"))
 	DB.add_crime(category, date, latitude, longitude, description)
 	return home()
+
+def format_date(userdate):
+	date = dateparser.parse(userdate)
+	try:
+		return datetime.datetime.strftime(date, "%Y-%m-%d")
+	except TypeError:
+		return None
+
+def sanitize_string(userinput):
+	whitelist = string.letters + string.digits + "!?$.,;:-'()&"
+	return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
